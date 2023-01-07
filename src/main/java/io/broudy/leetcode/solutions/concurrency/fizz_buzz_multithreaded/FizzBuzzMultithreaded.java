850741c70869e27913668ad6e49dff812d46aec9
Julian Broudy <Julian@Broudy.io>
2023-01-07 22:11:13 +0200

    Formatting.

--- src/main/java/io/broudy/leetcode/solutions/concurrency/fizz_buzz_multithreaded/FizzBuzzMultithreaded.java
+++ src/main/java/io/broudy/leetcode/solutions/concurrency/fizz_buzz_multithreaded/FizzBuzzMultithreaded.java
@@ -7,24 +7,29 @@ package io.broudy.leetcode.solutions.concurrency.fizz_buzz_multithreaded;
  */
 public class FizzBuzzMultithreaded {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        final int n = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz(n);
+		final int n = 15;
+		FizzBuzz fizzBuzz = new FizzBuzz(n);
 
-        FizzBuzzMultithreadedThread a = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.FIZZ, () -> System.out.print("Fizz, "));
-        FizzBuzzMultithreadedThread b = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.BUZZ, () -> System.out.print("Buzz, "));
-        FizzBuzzMultithreadedThread c = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.FIZZBUZZ, () -> System.out.print("FizzBuzz, "));
-        FizzBuzzMultithreadedThread d = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.NUMBER, () -> System.out.println("Number"));
+		FizzBuzzMultithreadedThread a = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.FIZZ,
+		                                                                () -> System.out.print("Fizz, "));
+		FizzBuzzMultithreadedThread b = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.BUZZ,
+		                                                                () -> System.out.print("Buzz, "));
+		FizzBuzzMultithreadedThread c = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.FIZZBUZZ,
+		                                                                () -> System.out.print("FizzBuzz, "));
+		FizzBuzzMultithreadedThread d = new FizzBuzzMultithreadedThread(fizzBuzz, FizzBuzzMethods.NUMBER,
+		                                                                () -> System.out.println("Number"));
 
-        d.start();
-        a.start();
-        b.start();
-        c.start();
-    }
+		d.start();
+		a.start();
+		b.start();
+		c.start();
 
-    public enum FizzBuzzMethods {
-        FIZZ, BUZZ, FIZZBUZZ, NUMBER
-    }
+	}
+
+	public enum FizzBuzzMethods {
+		FIZZ, BUZZ, FIZZBUZZ, NUMBER
+	}
 
 }
