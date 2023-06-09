1698a12d4e4a49049ba0d11a2f4c84bbfb7c3368
Julian Broudy <Julian@Broudy.io>
2023-06-09 18:04:21 +0300

    169. Majority Element O(1) space complexity

--- src/main/java/io/broudy/leetcode/solutions/MajorityElement.java
+++ src/main/java/io/broudy/leetcode/solutions/MajorityElement.java
@@ -1,33 +1,26 @@
 package io.broudy.leetcode.solutions;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <a href="https://leetcode.com/problems/majority-element/">169. Majority Element</a>.
  *
  * @author <a href="https://github.com/JulianBroudy">Julian Broudy</a>
  */
 public class MajorityElement {
-    public static void main(String[] args) {
-        int[] intArray = new int[]{3,2,3,2,2,3,2};
-        System.out.println(majorityElement(intArray));
-    }
+	public static void main(String[] args) {
+		int[] intArray = new int[]{3, 3, 4};
+		System.out.println(majorityElement(intArray));
+	}
 
-    private static int majorityElement(int[] nums) {
-        Map<Integer, Integer> numberToCountMapping = new HashMap<>();
-        for (int num : nums) {
-            numberToCountMapping.compute(num, (k, v) -> v == null ? 0 : v + 1);
-        }
-        int majorityElement = nums[0];
-        int lastMajorityCount = 0;
-        for (Map.Entry<Integer, Integer> entry : numberToCountMapping.entrySet()) {
-            if(entry.getValue()>lastMajorityCount){
-                lastMajorityCount = entry.getValue();
-                majorityElement = entry.getKey();
-            }
-        }
-        return majorityElement;
-    }
-    
+	private static int majorityElement(int[] nums) {
+		int majorityElement = nums[0];
+		int countOfCurrentMajorityElement = 0;
+		for (int num : nums) {
+			if (countOfCurrentMajorityElement == 0) {
+				majorityElement = num;
+			}
+			countOfCurrentMajorityElement += majorityElement == num ? 1 : -1;
+		}
+		return majorityElement;
+	}
+
 }
